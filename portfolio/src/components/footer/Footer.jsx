6d46b0d44b1b4c0bf1e16e955d227655e71c457b
shankarr-rm <shankarrr.rm@gmail.com>
2024-07-09 21:39:30 +0530

    Created the footer component

---
+++ portfolio/src/components/footer/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Footer.css'
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
+
+
+const Footer = () => {
+  return (
+      <div className='footer'>
+  
+      <a href="#" className='footer__logo'>SHANKAR MAHENDRAN</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebookF /></a>
+        <a href="https://instagram.com"><FiInstagram /></a>
+        <a href="https://twitter.com"><IoLogoTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+          <small>&copy; SHANKAR 2024. All rights reserved</small>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
