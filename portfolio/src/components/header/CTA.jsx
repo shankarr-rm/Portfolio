26fc859754c62513c630102e84c3b057f535f43d
Shankar Mahendran <shankarrr.rm@gmail.com>
2026-05-20 21:54:12 +0530

    Added addtional features

--- portfolio/src/components/header/CTA.jsx
+++ portfolio/src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import Shankar_CV from '../../assets/Shankar_CV.pdf'
 
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
+      <a href={Shankar_CV} download className='btn'>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
     </div>
--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import './Header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
+import { Typewriter } from 'react-simple-typewriter';
 
 const Header = () => {
   return (
@@ -11,7 +12,20 @@ const Header = () => {
         <div className="introduction">
         <h2>Hello I'm</h2>
         <h1>Shankar Mahendran</h1>
-        <h5 className="test_light"> Fullstack Developer</h5>
+        {/* <h5 className="test_light"> Fullstack Developer</h5> */}
+        <h5 className="test_light"><Typewriter
+              words={[
+                'Fullstack Developer',
+                'DevOps Engineer',
+                'UI/UX Enthusiast'
+                     ]}
+              loop={0}
+              cursor
+              typeSpeed={80}
+              deleteSpeed={50}
+              delaySpeed={1500}
+            />
+        </h5>
         <h4 className="caption_about">I'm a full stack developer with a passion for creating dynamic and responsive websites. 
         I bring ideas to life with clean, efficient code and innovative design.</h4>
         <CTA/>
